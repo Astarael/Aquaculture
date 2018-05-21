6713aa81b36a5677d77f9f6869db10a8ad6fddb8
laeneris <samuel.r.baxter@gmail.com>
2018-05-21 21:05:58 +1000

    Basic mod construction

---
+++ src/main/java/com/astarael/aquaponics/CommonProxy.java
@@ -0,0 +1,54 @@
+package com.astarael.aquaponics;
+
+import com.astarael.aquaponics.Blocks.*;
+import com.astarael.aquaponics.Items.*;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.io.File;
+
+@Mod.EventBusSubscriber
+public class CommonProxy {
+
+    // Config instance
+    public static Configuration config;
+
+    public void preInit(FMLPreInitializationEvent e) {
+        File directory = e.getModConfigurationDirectory();
+        config = new Configuration(new File(directory.getPath(), "modtut.cfg"));
+        Config.readConfig();
+    }
+
+    public void init(FMLInitializationEvent e) {
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+        if (config.hasChanged()) {
+            config.save();
+        }
+    }
+
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+        event.getRegistry().register(new LimeKiln());
+    }
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(new ItemBlock(ModBlocks.limeKiln).setRegistryName(ModBlocks.limeKiln.getRegistryName()));
+        event.getRegistry().register(new FishCod());
+    }
+}
+
+
+
+
+
