21de31f8b73c66f2fe9e1df7d4d9e45e8f5629ea
Astarael <samuel.r.baxter@gmail.com>
2018-07-24 22:29:49 +1000

    Added basic Vat code

--- src/main/java/com/astarael/aquaculture/TileEntitities/Vat.java
+++ src/main/java/com/astarael/aquaculture/TileEntitities/Vat.java
@@ -2,6 +2,7 @@ package com.astarael.aquaculture.TileEntitities;
 
 import com.astarael.aquaculture.Aquaculture;
 import jline.internal.Nullable;
+import net.minecraft.block.BlockCauldron;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.EntityLivingBase;
